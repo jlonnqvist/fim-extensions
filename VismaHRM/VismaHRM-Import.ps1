f8dea6906220d786ba57e088d31dc225a5bd1b10
Kim Berg <kimberg88@gmail.com>
2018-02-09 16:51:08 +0100

    Groups are now based on the Organizational Chart
    * Groups are now based on the Organizational Chart (instead of costCentre)
    * The user-objects Anchor has been changed from EmployeeID to VismaHRMID in order to add the department manager to the respective department group (REQUIRES SCHEMA REFRESH!)
    * Some other minor changes

--- VismaHRM/VismaHRM-Import.ps1
+++ VismaHRM/VismaHRM-Import.ps1
@@ -23,51 +23,35 @@ $DaysUntillStart = 30 # Get employees that have not started yet as well
 $Log = "log-import.txt"
 
 #******************************************************************************
-# ABOUT: Version: 1.0, Author: kimberg88@gmail.com
+# ABOUT: Version: 1.9, Author: kimberg88@gmail.com
 # REQUIREMENT: Webservice: "Visma HRM-WS"
 
 
 Set-Location $(split-path -parent $MyInvocation.MyCommand.Definition) # Set working directory to script directory
-$Groups = @{}
+$Culture = (Get-Culture).TextInfo
+
+$global:GroupsOC = @()
 $global:ReturnedUsers = 0
 
-Function ProjectGroups($Groups) {
-    Foreach ($Group in $Groups.GetEnumerator()) {
-        $GroupName = "FIM-VISMA." + $Group.Key
 
-        $GroupName = $GroupName.Replace("/", " ").Replace("\", " ").Replace(":", " ").Replace(","," ") # Trim unwanted characters
-
-        if ($GroupName.Length -gt 60) { # Max CN limit in Active Directory is 64
-            $GroupName = $GroupName.Substring(0,60) 
-        }
-
-        $obj = @{}
-        $obj.add("Id", $GroupName)
-        $obj.add("objectClass", "group")
-        $obj.add("displayName", $GroupName)
-        $obj.add("HRM_ADPath", $ADPathGroups)
-        $obj.add("Member", $Group.Value) 
-        $obj 
-    }
-}
-
-Function ProjectUsers($URI, $OnlyFutureEmployees) {
+Function ProjectUsers($XMLPath, $OnlyFutureEmployees) {
     try {
 	    "[$(Get-Date)] Request URI: $($URI)" | Out-File $Log -Append
-	
-        $Culture = (Get-Culture).TextInfo
+		$Culture = (Get-Culture).TextInfo
         $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
         $Credentials = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
 
         $WebPage = Invoke-RestMethod $URI -Credential $Credentials -TimeoutSec $WebRequestTimeout
-	    $EmployeeXML =  $WebPage.personsXML.person
+	    $XML =  $WebPage.personsXML.person
 	
-        Foreach ($Employee in $EmployeeXML) {
+        Foreach ($Employee in $XML) {
+			$VismaID = $Employee.personIdHRM
             $EmployeeID = $Employee.employments.employment.employeeId.ToString()
             $EmployeeStart = $Employee.employments.employment.startDate
 			$Department = "Ingen Avdeling"
 			$PrimaryDepartment = $NULL
 			$JobTitle = $NULL
+			$ManagerID = $NULL
 			
             "[$(Get-Date)] Process employee: $($EmployeeID)" | Out-File $Log -Append
 
@@ -82,34 +66,56 @@ Function ProjectUsers($URI, $OnlyFutureEmployees) {
 				# Skip jobs that have not yet started
                 if ([DateTime]$Job.validFromDate -gt $(Get-Date)) { continue } 
 
-                $Department = $Job.costCentres.dimension2.name
+                $Department = $Job.chart.unit.id
+				try {
+					if ($Department) {	
+						# Store department membership information in an array that we can parse later...
+						$StoredGroup = $global:GroupsOC | Where { $_.ID -eq $Job.chart.unit.id }
+						if ($StoredGroup) {
+							if(-not ($StoredGroup.Members -contains $VismaID)) { 
+								$StoredGroup.Members += $VismaID
+							}
+						} else {
+							if($Job.chart.unit.manager.id) {
+								$Members = @($Job.chart.unit.manager.id, $VismaID) # Add the manager as well
+							} else {
+								$Members = @($VismaID)
+							}
+							$global:GroupsOC += @{
+								ID = $Job.chart.unit.id
+								Name = $Job.chart.unit.name
+								Members = $Members
+							}
+						}
 
-                if ($Department) {
-                    If($Groups.Count -gt 0) { # If we do a 'get_item' when $Groups are empty it will crash and burn
-                        If($Groups.Get_Item($Department)) {
-                            if(-not $Groups.Get_Item($Department).Contains($EmployeeID)) {
-                                $Members = $Groups.Get_Item($Department) + $EmployeeID
-                                $Groups.Set_Item($Department, $Members)
-                            }
-                        } else { 
-                            $Groups.Add($Department, @($EmployeeID))
-                        }
-                    } else {
-                        $Groups.Add($Department, @($EmployeeID))
-                    }
+						if ($Job.isPrimaryPosition -eq "true") {
+							$PrimaryDepartment = $Job.chart.unit.name
+							
+							$JobTitle = $Job.positionStatistics.workClassification.name
+							
+							if ($JobTitle.Contains("Rektor")) { $JobTitle = $JobTitle.substring(0, $JobTitle.indexOf("(")) }
+							if ($JobTitle.Contains("Uten godkjent")) { $JobTitle = $JobTitle.substring(0, $JobTitle.indexOf("(")) }
+							if ($JobTitle.Contains("vikar (grunnskole)")) { $JobTitle = "Laerer" } # Encoding issue with Æ
+							if ($JobTitle.Contains("Foster")) { $JobTitle = " " }
 
-                    if ($Job.isPrimaryPosition -eq "true") {
-						$PrimaryDepartment = $Department
-                        $JobTitle = $Job.positionStatistics.workClassification.name
-                    }
-                }
+							if ($JobTitle) {
+								$JobTitle = $JobTitle.Replace("&","og").Replace("/", " ").Replace("\", " ")
+							}
+						}
+
+					}
+				} catch {
+					"Ingen info om avdeling" | Out-File $Log -Append
+				}
             }
+			
 			
 			$CurrentAliases = $Employee.authentication.alias
 			if ($CurrentAliases) { $CurrentAliases = $CurrentAliases.toLower() }
 			
 			$obj = @{}
-		    $obj.add("HRM_employeeID", $EmployeeID)
+		    $obj.add("HRM_EmployeeID", $EmployeeID)
+			$obj.add("HRM_VismaHRMID", $VismaID)
 		    $obj.add("objectClass", "user")
             $obj.add("HRM_ssn", $Employee.ssn.toString())
 			$obj.add("HRM_UserID", $Employee.authentication.userId.toString())
@@ -120,10 +126,22 @@ Function ProjectUsers($URI, $OnlyFutureEmployees) {
             $obj.add("HRM_type", "employee")
 			$obj.add("HRM_mainDepartment", $PrimaryDepartment)
 			$obj.add("HRM_jobtitle", $JobTitle)
+			$obj.add("HRM_ManagerID", $ManagerID)
             $obj.add("HRM_ADPath", $ADPathEmployees)
 			$obj.add("HRM_ADPathDisabled", $ADPathDisabledEmployees)
 			$obj.add("HRM_ADDomain", $ADDomain)
-			$obj.add("HRM_comment", "FIM-VISMA : Ansatt $($EmployeeStart) : $Department")
+			$obj.add("HRM_ADLDSPath", $ADLDSPathEmployees)
+			$obj.add("HRM_ADLDSDomain", $ADLDSDomain)
+			$obj.add("HRM_Affilliation", ("member", "employee", "staff"))
+			$obj.add("HRM_Entitlement", $NULL)
+			$obj.add("HRM_MemberOfOrganization", $OrgNr)
+			$obj.add("HRM_comment", "FIM-VISMA : $($PrimaryDepartment)")
+			if ($Employee.contactInfo.mobilePhone) {
+				$obj.add("HRM_CellphoneWork", $Employee.contactInfo.mobilePhone.toString())
+			}
+			if ($Employee.contactInfo.privateMobilePhone) {
+				$obj.add("HRM_CellphonePrivate", $Employee.contactInfo.privateMobilePhone.toString())
+			}
             $obj
 			
 			$global:ReturnedUsers++
@@ -136,13 +154,34 @@ Function ProjectUsers($URI, $OnlyFutureEmployees) {
 }
 
 
+Function ProjectGroups($GroupsOC) {
+    Foreach ($Group in $GroupsOC) {
+        $GroupName = "FIM-VISMA." + $Group.Name
+		$GroupName = $GroupName.Replace("/", " ").Replace("\", " ").Replace(":", " ").Replace(","," ")
+
+        if ($GroupName.Length -gt 60) { # Max CN limit in Active Directory is 64
+            $GroupName = $GroupName.Substring(0,60) 
+        }
+
+        $obj = @{}
+        $obj.add("Id", $Group.ID)
+        $obj.add("objectClass", "group")
+        $obj.add("displayName", $GroupName)
+        $obj.add("HRM_ADPath", $ADPathGroups)
+        $obj.add("Member", $Group.Members) 
+        $obj 
+    }
+}
+
+
+
 $URI = "http://$($Server)/hrm_ws/secure/persons/company/1/start-id/$($EmployeeStartID)/end-id/$($EmployeeEndID)"
 ProjectUsers $URI $False
 
 $URI = "http://$($Server)/hrm_ws/secure/persons/company/1/not-started/date-interval/$((Get-Date).AddDays(1).ToString('yyyy-MM-dd'))/$((Get-Date).AddDays($DaysUntillStart).ToString('yyyy-MM-dd'))"
 ProjectUsers $URI $True
 
-ProjectGroups $Groups
+ProjectGroups $GroupsOC
 
 
 if ($ReturnedUsers -lt $FailSafe) {
--- VismaHRM/VismaHRM-Schema.ps1
+++ VismaHRM/VismaHRM-Schema.ps1
@@ -1,6 +1,7 @@
 $obj = New-Object -Type PSCustomObject
-$obj | Add-Member -Type NoteProperty -Name "Anchor-HRM_EmployeeID|String" -Value 1
+$obj | Add-Member -Type NoteProperty -Name "Anchor-HRM_VismaHRMID|String" -Value 1
 $obj | Add-Member -Type NoteProperty -Name "objectClass|String" -Value "user"
+$obj | Add-Member -Type NoteProperty -Name "HRM_EmployeeID|String" -Value "99"
 $obj | Add-Member -Type NoteProperty -Name "HRM_ssn|String" -Value "99"
 $obj | Add-Member -Type NoteProperty -Name "HRM_UserID|String" -Value "99"
 $obj | Add-Member -Type NoteProperty -Name "HRM_Alias|String[]" -Value "USERNAME"
@@ -9,14 +10,20 @@ $obj | Add-Member -Type NoteProperty -Name "HRM_lastname|String" -Value "Granfel
 $obj | Add-Member -Type NoteProperty -Name "HRM_fullname|String" -Value "Soren Granfeldt"
 $obj | Add-Member -Type NoteProperty -Name "HRM_mainDepartment|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_jobtitle|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_ManagerID|Reference" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_type|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_comment|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_Mail|String" -Value "Active"
-$obj | Add-Member -Type NoteProperty -Name "HRM_TelephoneWork|String" -Value "Active"
-$obj | Add-Member -Type NoteProperty -Name "HRM_TelephonePrivate|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_CellphoneWork|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_CellphonePrivate|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_ADPath|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_ADPathDisabled|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "HRM_ADDomain|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_ADLDSDomain|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_Affilliation|String[]" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_Entitlement|String" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_MemberOfOrganization|Reference" -Value "Active"
+$obj | Add-Member -Type NoteProperty -Name "HRM_ADLDSPath|String" -Value "Active"
 $obj | Add-Member -Type NoteProperty -Name "MemberOf|Reference[]" -Value (2,3)
 $obj
 
